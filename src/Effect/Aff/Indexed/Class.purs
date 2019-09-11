a188ad187a5b1a4bcfbb8ba9cfbe09f267cb0d98
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-09-10 17:38:16 -0700

    Fix compiler issues

--- src/Effect/Aff/Indexed/Class.purs
+++ src/Effect/Aff/Indexed/Class.purs
@@ -2,14 +2,13 @@ module Effect.Aff.Indexed.Class where
 
 import Prelude
 
-import Control.Monad.Indexed (class IxMonad)
 import Data.Indexed (Indexed(..))
 import Effect.Aff (Aff)
-import Effect.Class (class MonadEffect)
+import Effect.Indexed.Class (class IxMonadEffect)
 import Effect.Aff.Class (class MonadAff, liftAff)
 
 class IxMonadEffect m <= IxMonadAff m where
-  iliftAff :: forall x a. Effect a -> m x x a
+  iliftAff :: forall x a. Aff a -> m x x a
 
 instance ixMonadAffIndexed :: (MonadAff m) => IxMonadAff (Indexed m) where
   iliftAff = Indexed <<< liftAff
